5bb8775d19bb6049464c08c03a923ab16df62eee
Sara Santos <santos.sara96@gmail.com>
2016-10-04 11:48:39 +0100

    oi  mano fizemos totil hoje

--- WebCGF.XMLReaderExample/reader/MySceneGraph.js
+++ WebCGF.XMLReaderExample/reader/MySceneGraph.js
@@ -34,6 +34,20 @@ MySceneGraph.prototype.onXMLReady=function()
 		return;
 	}	
 
+    error = this.parsePrimitives(rootElement);
+
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
+
+	error = this.parseTransformations(rootElement);
+
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
+
 	this.loadedOk=true;
 	
 	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
@@ -65,14 +79,91 @@ MySceneGraph.prototype.parseGlobals= function(rootElement) {
 	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
 };
 
+MySceneGraph.prototype.parseTransformations = function(rootElement) {
+	var transformations = rootElement.getElementsByTagName('transformations');
+	if(transformations == null){
+		return "transformations element is missing.";
+	}
+
+	if (transformations.length < 1) {
+		return "zero 'transformations' element found.";
+	}
+
+	var listTransformations = transformations[0].getElementsByTagName('transformation');
+
+	if(listTransformations== null){
+		return "transformation element is missing.";
+	}
+
+	if (listTransformations.length < 1) {
+		return "zero 'transformation' element found.";
+	}
+
+	console.log("transformations size: " + listTransformations.length);
+
+	var i =0;
+	for(i; i< listTransformations.length; i++){
+		var transformation = listTransformations[i].children[0];
+		switch(transformation.tagName){
+			case "rotation":
+				console.log("Rotation angle= " + transformation.attributes.getNamedItem("angle").value);
+				break;
+			case "translate":
+				console.log("Translate x=" + transformation.attributes.getNamedItem("x").value);
+				break;
+			case "scale":
+				console.log("Scale x= " + transformation.attributes.getNamedItem("x").value);
+				break;
+			default: break;
+		}
+	}
+
+}
+
 MySceneGraph.prototype.parsePrimitives = function(rootElement) {
 	var primitives = rootElement.getElementsByTagName('primitives');
 	if(primitives == null){
 		return "primitives element is missing.";
 	}
 
-	if (primitives.length == 0) {
+	if (primitives.length < 1) {
 		return "zero 'primitives' element found.";
+	}
+
+	var listPrimitives = primitives[0].getElementsByTagName('primitive');
+
+	if(listPrimitives== null){
+		return "primitive element is missing.";
+	}
+
+	if (listPrimitives.length < 1) {
+		return "zero 'primitive' element found.";
+	}
+
+	console.log("primitives size: " + listPrimitives.length);
+
+	var i =0;
+	for(i; i< listPrimitives.length; i++){
+		var primitive = listPrimitives[i].children[0];
+		switch(primitive.tagName){
+			case "rectangle":
+				console.log("Rectangle x1= " + primitive.attributes.getNamedItem("x1").value);
+				break;
+			case "sphere":
+				console.log("sphere radius= " + primitive.attributes.getNamedItem("radius").value);
+				break;
+			case "triangle":
+				console.log("triangle x1= " + primitive.attributes.getNamedItem("x1").value);
+				break;
+			case "cylinder":
+				console.log("cylinder base= " + primitive.attributes.getNamedItem("base").value);
+				break;
+			case "torus":
+				console.log("torus inner= " + primitive.attributes.getNamedItem("inner").value);
+				break;
+
+			default: break;
+		}
 	}
 
 }
