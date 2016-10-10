c896e817b3b594805c83f9f5c9c9fd6fc50543ac
Sara Santos <santos.sara96@gmail.com>
2016-10-10 18:03:30 +0100

    xml scene update

--- WebCGF.XMLReaderExample/reader/MySceneGraph.js
+++ WebCGF.XMLReaderExample/reader/MySceneGraph.js
@@ -28,21 +28,48 @@ MySceneGraph.prototype.onXMLReady=function()
 	
 	// Here should go the calls for different functions to parse the various blocks
 	var error = this.parseGlobals(rootElement);
-
 	if (error != null) {
 		this.onXMLError(error);
 		return;
 	}	
 
-    error = this.parsePrimitives(rootElement);
+	error = this.parseViews(rootElement);
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
 
+	error = this.parseIlumination(rootElement);
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
+
+	error = this.parseLights(rootElement);
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
+
+	error = this.parseTextures(rootElement);
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
+
+	error = this.parseMaterials(rootElement);
 	if (error != null) {
 		this.onXMLError(error);
 		return;
 	}
 
 	error = this.parseTransformations(rootElement);
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}
 
+    error = this.parsePrimitives(rootElement);
 	if (error != null) {
 		this.onXMLError(error);
 		return;
@@ -53,8 +80,6 @@ MySceneGraph.prototype.onXMLReady=function()
 	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
 	this.scene.onGraphLoaded();
 };
-
-
 
 /*
  * Example of method that parses elements of one block and stores information in a specific data structure
@@ -76,30 +101,152 @@ MySceneGraph.prototype.parseGlobals= function(rootElement) {
 	this.cullface = this.reader.getItem(globals, 'cullface', ["back","front","none", "frontandback"]);
 	this.cullorder = this.reader.getItem(globals, 'cullorder', ["ccw","cw"]);
 
-	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
+	//console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
+};
+
+MySceneGraph.prototype.parseViews = function(rootElement) {
+	var views = rootElement.getElementsByTagName('views');
+	if(views == null || views.length <1){
+		return "views element is missing.";
+	}
+
+	var default_view = views[0].attributes.getNamedItem("default").value;
+	var listPerspectives = views[0].getElementsByTagName('perspective');
+	//console.log(listPerspectives.length);
+
+	var i =0;
+	for(i; i< listPerspectives.length; i++){
+		var id, near, far, angle, from, to;
+		id= listPerspectives[i].attributes.getNamedItem("id").value;
+		near= listPerspectives[i].attributes.getNamedItem("near").value;
+		far= listPerspectives[i].attributes.getNamedItem("far").value;
+		angle= listPerspectives[i].attributes.getNamedItem("angle").value;
+		from = listPerspectives[i].getElementsByTagName('from')[0];  
+		to = listPerspectives[i].getElementsByTagName('to')[0];
+		//console.log(id + " " + near +" " + far+ " "+angle);
+
+		//TODO Guardar os valores de cada tag
+	}	
+};
+
+MySceneGraph.prototype.parseIlumination = function(rootElement) {
+	var ilumination = rootElement.getElementsByTagName('ilumination');
+
+	if(ilumination == null || ilumination.length <1){
+		return "ilumination element is missing.";
+	}
+
+	var ambient = ilumination[0].getElementsByTagName('ambient')[0];
+	if(ambient == null){
+		return 'ambient element is missing';
+	}
+	//console.log(ambient.attributes.getNamedItem("r").value);
+
+	var background = ilumination[0].getElementsByTagName('background')[0];
+	if(background == null){
+		return 'background element is missing';
+	}
+	//console.log(background.attributes.getNamedItem("r").value);
+};
+
+MySceneGraph.prototype.parseLights = function(rootElement) {
+	var lights = rootElement.getElementsByTagName('lights');
+	if(lights == null || lights.length <1){
+		return "lights element is missing.";
+	}
+
+	//Deals with omni blocks 
+	var omni = lights[0].getElementsByTagName('omni');
+	var i=0;
+	for(i;i<omni.length;i++){
+		var id, enabled;
+		id = omni[i].attributes.getNamedItem('id').value;
+		enabled = omni[i].attributes.getNamedItem('enabled').value;
+
+		//Gets the different tags
+		var location, ambient, difuse, specular;
+		location = omni[i].getElementsByTagName('location')[0];
+		ambient = omni[i].getElementsByTagName('ambient')[0];
+		difuse = omni[i].getElementsByTagName('difuse')[0];
+		specular = omni[i].getElementsByTagName('specular')[0];
+
+		//TODO Guardar os valores de cada tag
+	}
+
+	var spot = lights[0].getElementsByTagName('spot');
+	var i=0;
+	for(i;i<omni.length;i++){
+		var id, enabled, angle, exponent;
+		id = spot[i].attributes.getNamedItem('id').value;
+		enabled = spot[i].attributes.getNamedItem('enabled').value;
+		angle = spot[i].attributes.getNamedItem('angle').value;
+		exponent = spot[i].attributes.getNamedItem('exponent').value;
+
+		//Gets the different tags
+		var target, location, ambient, difuse, specular;
+		target = omni[i].getElementsByTagName('target')[0];
+		location = omni[i].getElementsByTagName('location')[0];
+		ambient = omni[i].getElementsByTagName('ambient')[0];
+		difuse = omni[i].getElementsByTagName('difuse')[0];
+		specular = omni[i].getElementsByTagName('specular')[0];
+
+		//TODO Guardar os valores de cada tag
+	}
+
+};
+
+MySceneGraph.prototype.parseTextures = function(rootElement) {
+	var textures = rootElement.getElementsByTagName('textures');
+	if(textures == null || textures.length <1){
+		return "textures element is missing.";
+	}
+
+	var listTextures = textures[0].getElementsByTagName('texture');
+	var i=0;
+	for(i;i<listTextures.length;i++){
+		var texture, id, file, length_s, length_t;
+		texture = listTextures[i];
+		id = texture.attributes.getNamedItem('id').value;
+		file = texture.attributes.getNamedItem('file').value;
+		length_s = texture.attributes.getNamedItem('length_s').value;
+		length_t = texture.attributes.getNamedItem('length_t').value;
+		//console.log(id +" "+file+" "+" "+length_s+" "+length_t);
+	}
+};
+
+MySceneGraph.prototype.parseMaterials = function(rootElement) {
+	var materials = rootElement.getElementsByTagName('materials');
+	if(materials == null || materials.length <1){
+		return "materials element is missing.";
+	}
+	
+	var listMaterials = materials[0].getElementsByTagName('material');
+	var i=0;
+	for(i; i< listMaterials.length;i++){
+		var id, emission, ambient, diffuse, specular, shininess;
+		id = listMaterials[i].attributes.getNamedItem('id').value;
+		emission = listMaterials[i].getElementsByTagName('emission');
+		ambient = listMaterials[i].getElementsByTagName('ambient');
+		difuse = listMaterials[i].getElementsByTagName('difuse');
+		specular = listMaterials[i].getElementsByTagName('specular');
+		shininess = listMaterials[i].getElementsByTagName('shininess');
+
+		//TODO Guardar valores de cada tag
+	}
 };
 
 MySceneGraph.prototype.parseTransformations = function(rootElement) {
 	var transformations = rootElement.getElementsByTagName('transformations');
-	if(transformations == null){
+	if(transformations == null || transformations.length <1){
 		return "transformations element is missing.";
 	}
 
-	if (transformations.length < 1) {
-		return "zero 'transformations' element found.";
-	}
-
 	var listTransformations = transformations[0].getElementsByTagName('transformation');
-
-	if(listTransformations== null){
-		return "transformation element is missing.";
+	if(listTransformations == null || listTransformations.length <1){
+		return "transformations element is missing.";
 	}
 
-	if (listTransformations.length < 1) {
-		return "zero 'transformation' element found.";
-	}
-
-	console.log("transformations size: " + listTransformations.length);
+	//console.log("transformations size: " + listTransformations.length);
 
 	var i =0;
 	for(i; i< listTransformations.length; i++){
@@ -110,45 +257,33 @@ MySceneGraph.prototype.parseTransformations = function(rootElement) {
 			var transformation = listTransformations[i].children[j];
 			switch(transformation.tagName){
 				case "rotation":
-					console.log("Rotation angle= " + transformation.attributes.getNamedItem("angle").value);
 					break;
 				case "translate":
-					console.log("Translate x=" + transformation.attributes.getNamedItem("x").value);
 					break;
 				case "scale":
-					console.log("Scale x= " + transformation.attributes.getNamedItem("x").value);
 					break;
 				default: break;
 			}
 		}
 	}
-
-}
+};
 
 MySceneGraph.prototype.parsePrimitives = function(rootElement) {
 	var primitives = rootElement.getElementsByTagName('primitives');
-	if(primitives == null){
+	if(primitives == null || primitives.length <1){
 		return "primitives element is missing.";
 	}
 
-	if (primitives.length < 1) {
-		return "zero 'primitives' element found.";
-	}
-
 	var listPrimitives = primitives[0].getElementsByTagName('primitive');
-
-	if(listPrimitives== null){
+	if(listPrimitives == null || listPrimitives.length <1){
 		return "primitive element is missing.";
 	}
 
-	if (listPrimitives.length < 1) {
-		return "zero 'primitive' element found.";
-	}
-
-	console.log("primitives size: " + listPrimitives.length);
+	//console.log("primitives size: " + listPrimitives.length);
 
 	var i =0;
 	for(i; i< listPrimitives.length; i++){
+		//console.log(listPrimitives[i].attributes.getNamedItem("id"));
 		var primitive = listPrimitives[i].children[0];
 		switch(primitive.tagName){
 			case "rectangle":
@@ -158,26 +293,20 @@ MySceneGraph.prototype.parsePrimitives = function(rootElement) {
 				var y2 =primitive.attributes.getNamedItem("y2").value;
 
 				var rectangle = new MySquare(this.scene, x1,y1,x2,y2);
-				console.log("Rectangle x1= " + primitive.attributes.getNamedItem("x1").value);
 				break;
 			case "sphere":
-				console.log("sphere radius= " + primitive.attributes.getNamedItem("radius").value);
 				break;
 			case "triangle":
-				console.log("triangle x1= " + primitive.attributes.getNamedItem("x1").value);
 				break;
 			case "cylinder":
-				console.log("cylinder base= " + primitive.attributes.getNamedItem("base").value);
 				break;
 			case "torus":
-				console.log("torus inner= " + primitive.attributes.getNamedItem("inner").value);
 				break;
 
 			default: break;
 		}
 	}
-
-}
+};
 	
 /*
  * Callback to be executed on any read error
