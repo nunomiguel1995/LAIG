1998460c8a316a85972b50fa32f911db24f38fb0
Nuno Miguel <nuno.migcastro@gmail.com>
2016-10-18 12:29:29 +0100

    Added sphere to the primitives

--- WebCGF.XMLReaderExample/reader/primitives/MyCylinder.js
+++ WebCGF.XMLReaderExample/reader/primitives/MyCylinder.js
@@ -6,7 +6,6 @@
 function MyCylinder(scene, base, top, height, slices, stacks) {
   CGFobject.call(this,scene);
 
-
   this.surface = new MyCylinderSurface(scene, base, top, height, slices, stacks);
   this.topCircle = new MyCylinderCircle(scene, top, slices);
   this.baseCircle = new MyCylinderCircle(scene, base, slices);
@@ -21,10 +20,12 @@ MyCylinder.prototype.display = function() {
   this.surface.display();
 
   this.scene.pushMatrix();
+    this.scene.rotation(Math.PI, 0, 0, 1);
     this.base.display();
   this.scene.popMatrix();
 
   this.scene.pushMatrix();
+    this.scene.translate(0, this.height, 0);
     this.top.display();
   this.scene.popMatrix();
 }
