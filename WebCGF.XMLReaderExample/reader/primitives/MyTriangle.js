05e29d956293ad27052655068de7378a50dd781c
nunomiguel1995 <nuno.migcastro@gmail.com>
2016-10-23 16:09:30 +0100

    Textures on triangles working

--- WebCGF.XMLReaderExample/reader/primitives/MyTriangle.js
+++ WebCGF.XMLReaderExample/reader/primitives/MyTriangle.js
@@ -23,11 +23,21 @@
 
  	this.indices = [0, 1, 2];
 
+
+	var p1 = this.point0;
+	var p2 = this.point1;
+	var p3 = this.point2;
+
+	var x = ((p2[1] - p1[1])*(p3[2] - p1[2]) - (p2[2] - p1[2])*(p3[1] - p1[1]))/Math.sqrt(Math.pow((p2[1] - p1[1])*(p3[2] - p1[2]) - (p2[2] - p1[2])*(p3[1] - p1[1]),2) + Math.pow((p2[2] - p1[2])*(p3[0] - p1[0]) - (p2[0] - p1[0]) * (p3[2] - p1[2]),2) + Math.pow((p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0]),2));
+    var y = ((p2[2] - p1[2])*(p3[0] - p1[0]) - (p2[0] - p1[0])*(p3[2] - p1[2]))/Math.sqrt(Math.pow((p2[1] - p1[1])*(p3[2] - p1[2]) - (p2[2] - p1[2])*(p3[1] - p1[1]), 2) + Math.pow((p2[2] - p1[2])*(p3[0] - p1[0]) - (p2[0] - p1[0])*(p3[2] - p1[2]),2) + Math.pow((p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0]),2));
+    var z = ((p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0]))/Math.sqrt(Math.pow((p2[1] - p1[1])*(p3[2] - p1[2])- (p2[2] - p1[2])*(p3[1] - p1[1]), 2) + Math.pow((p2[2] - p1[2])*(p3[0] - p1[0]) - (p2[0] - p1[0])*(p3[2] - p1[2]),2) + Math.pow((p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0]), 2));
+
+
  	var dA = vec3.create();
- 	vec3.sub(dA, this.point2, this.point0);
+ 	vec3.sub(dA, this.point1, this.point0);
 
  	var dB = vec3.create();
- 	vec3.sub(dB, this.point1, this.point0);
+ 	vec3.sub(dB, this.point2, this.point0);
 
  	var dC = vec3.create();
  	vec3.sub(dC, this.point2, this.point1);
@@ -40,15 +50,14 @@
  					norm[0], norm[1], norm[2],
  					norm[0], norm[1], norm[2]];
 
- 	var a = vec3.length(dA);
- 	var c = vec3.length(dC);
- 	var cosBeta = (vec3.sqrLen(dA) - vec3.sqrLen(dB) + vec3.sqrLen(dC)) / (2 * a * c);
- 	var beta = Math.acos(this.cosBeta);
+    var u = (vec3.sqrLen(dA) + vec3.sqrLen(dB) - vec3.sqrLen(dC))/ (2 * vec3.length(dA));
+	var v = Math.sqrt(vec3.sqrLen(dB) - u * u);
 
-    this.texCoords = [0, 0,
-    				  c, 0,
-    				  c - a * Math.cos(beta), a * Math.sin(beta)];
-    				  
+	this.texCoords = [0,0,
+					  u,0,
+					  v, u
+	];
+
  	this.primitiveType = this.scene.gl.TRIANGLES;
  	this.initGLBuffers();
  };
